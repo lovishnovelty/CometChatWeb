8ba33e96a2ab04ac6cc0f967d13527dd0392073f
Suprimshk <suprim@noveltytechnology.com>
2023-01-17 17:57:49 +0545

    implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { CometChat } from '@cometchat-pro/chat';
+import { CometChatUI } from './cometchat-pro-react-ui-kit/CometChatWorkspace/src';
+const authKey = 'f7a9f594219a32a8177d445f037b59f2bfe963f4';
+const uid = 'p8voz';
+const receiverID = '1234';
+// const callType = 'VIDEO' | 'AUDIO' | undefined;
 function App() {
+  useEffect(() => {
+    CometChat.login(uid, authKey).then(
+      (user) => {
+        console.log('Login Successful:', { user });
+      },
+      (error) => {
+        console.log('Login failed with exception:', { error });
+      }
+    );
+
+    // CometChat.startCall(receiverID, 'video')
+    //   .then((call: any) => {
+    //     console.log('Call started successfully:', call);
+    //   })
+    //   .catch((error: any) => {
+    //     console.log('Error starting call:', error);
+    //   });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '800px', height: '800px' }}>
+      <CometChatUI />
     </div>
   );
 }
